8ec7b1a9fc74ddd4d2af7aad03b315c6fdb1c132
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-05-14 11:59:20 +0200

    test1.1.1.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,14 +10,11 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-//        JsonParser jsonParser = new JsonParser();
-//        JsonElement jsonTree = jsonParser.parse(request);
-//        int f1 = jsonTree.get("current_buy_in").getAsInt();
         System.out.println(request);
         JsonObject cuccok = request.getAsJsonObject();
         JsonElement buyIn = cuccok.get("current_buy_in");
 
-        System.out.println("Buy in:" + buyIn.toString());
+        System.out.println("FUCK Buy in:" + buyIn.toString());
         System.out.println("hello");
 
         return 1000;
