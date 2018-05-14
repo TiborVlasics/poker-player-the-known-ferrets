fb59cb55a8f7927b4e5da8a7bb9d14fdb92a97af
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-05-14 11:23:04 +0200

    use parser

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,8 +9,8 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonParser jsonParser = new JsonParser();
-        JsonElement jsonTree = parser.parse(request);
-        int f1 = jsonObject.get("current_buy_in").getAsInt();
+        JsonElement jsonTree = jsonParser.parse(request);
+        int f1 = jsonTree.get("current_buy_in").getAsInt();
         System.out.println(f1);
 
         return f1;
