7d8b619d0955d7db8c61efe0d05a1ed45a896f1f
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-05-14 13:40:34 +0200

    Get hole cards

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,11 +13,24 @@ public class Player {
         System.out.println(request);
         JsonObject cuccok = request.getAsJsonObject();
         JsonElement buyInJson = cuccok.get("current_buy_in");
+        JsonElement inActionJson = cuccok.get("in_action");
+        int inAction = inActionJson.getAsInt();
         int buyIn = buyInJson.getAsInt();
         JsonElement players = cuccok.get("players");
+        JsonArray playersArray = players.getAsJsonArray();
+        for (JsonElement element : playersArray) {
+            JsonObject object = players.getAsJsonObject();
+            if (object.get("id").getAsInt() == inAction) {
+                JsonElement holeCards = object.get("hole_cards");
+                JsonArray myHoleArray = holeCards.getAsJsonArray();
+                System.out.println("PLS WORKKKKK!!!");
+                System.out.println(myHoleArray);
+            }
 
+        }
 
-        System.out.println("FUCK PLAYERS:" + players);
+        System.out.println("FUCK PLAYERS:" + playersArray);
+        System.out.println("OUR PLAYERS ID:" + inAction);
 
         return buyIn;
     }
