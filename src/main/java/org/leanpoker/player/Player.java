63276fdac6c6bfb652e74c5a23eeea02bf15e156
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-05-14 10:36:58 +0200

    Bot returns current_by_in

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,14 +2,17 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 50;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonTree = parser.parse(request);
+        int f1 = jsonObject.get("current_buy_in").getAsInt();
+        System.out.println(f1);
+
+        return f1;
     }
 
     public static void showdown(JsonElement game) {
