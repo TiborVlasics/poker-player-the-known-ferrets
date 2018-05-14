f307acdabe7ff16fa21c3b7a6dfe78353873ec78
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-05-14 11:29:58 +0200

    test

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,10 +8,11 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        JsonParser jsonParser = new JsonParser();
+//        JsonParser jsonParser = new JsonParser();
 //        JsonElement jsonTree = jsonParser.parse(request);
 //        int f1 = jsonTree.get("current_buy_in").getAsInt();
         System.out.println(request);
+        System.out.println("hello");
 
         return 1000;
     }
